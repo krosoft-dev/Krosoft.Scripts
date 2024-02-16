602b1235dd2b40b90c9dbcda1e91e8ae80db3da1
Kévin ROBERT <kevin.robert.kr@gmail.com>
2024-02-16 20:20:22 +0100

    Ajout GitClone

--- powershell/modules/Krosoft/scripts/git.ps1
+++ powershell/modules/Krosoft/scripts/git.ps1
@@ -3,7 +3,7 @@ function GitVersion {
 }
 Set-Alias KGV GitVersion
 
-function GitClean {
+function GitPrune {
     Write-Host "=========================================="
     Write-Host "Clean repository"
     Write-Host "=========================================="
@@ -15,7 +15,7 @@ function GitClean {
     git branch -vv | Where-Object { $_ -match '\[origin/.*: gone\]' } | ForEach-Object { git branch -D ($_.split(" ", [StringSplitOptions]'RemoveEmptyEntries')[0]) }
     Write-Host  
 }
-Set-Alias KGC GitClean
+Set-Alias KGPr GitPrune
 
 function GitPull {
     Write-Host "=========================================="
@@ -27,4 +27,15 @@ function GitPull {
     git pull
     Write-Host        
 }
-Set-Alias KGP GitPull
+Set-Alias KGPu GitPull
+function GitClone($repository) {
+    Write-Host "=========================================="
+    Write-Host "Clone repository : "$repository
+    Write-Host "=========================================="
+    $path = Get-Location
+    Write-Host "Path : " $path
+    Write-Host "=========================================="
+    git clone $repository
+    Write-Host        
+}
+Set-Alias KGClo GitClone
