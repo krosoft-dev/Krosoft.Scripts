4208d825c4004f83c2256b99d525467d38696093
Kévin ROBERT <kevin.robert.kr@gmail.com>
2024-02-21 22:37:35 +0100

    maj

--- powershell/modules/Krosoft/scripts/ef.ps1
+++ powershell/modules/Krosoft/scripts/ef.ps1
@@ -16,6 +16,42 @@ function EfUpdateService($projet) {
 } 
 
 
+function EfRebuildService($projet) {   
+    Write-Host "Starting: EfRebuildService" -ForegroundColor Green
+    Write-Host "=============================================================================="    
+    $path = Get-Location
+    Write-Host "Projet                     : " $projet 
+    Write-Host "Path                       : " $path 
+    Write-Host "=============================================================================="  
+   
+    Set-Location $projet
+    $script_ef = ".\scripts\ef_rebuild-database.ps1" 
+    . $script_ef  
+    Set-Location $path  
+    Write-Host "Finishing: EfRebuildService" -ForegroundColor Green
+    Write-Host
+    Write-Host 
+} 
+
+
+function EfAddMigrationService($projet) {   
+    Write-Host "Starting: EfAddMigrationService" -ForegroundColor Green
+    Write-Host "=============================================================================="    
+    $path = Get-Location
+    Write-Host "Projet                     : " $projet 
+    Write-Host "Path                       : " $path 
+    Write-Host "=============================================================================="  
+   
+    Set-Location $projet
+    $script_ef = ".\scripts\ef_add-migration.ps1" 
+    . $script_ef  
+    Set-Location $path  
+    Write-Host "Finishing: EfAddMigrationService" -ForegroundColor Green
+    Write-Host
+    Write-Host 
+} 
+
+
 
 function EfUpdate( $context, $startupProject, $projet) { 
     Write-Host "================Update Database=========================="
@@ -40,4 +76,94 @@ function EfUpdateFromJson($json) {
     } 
 
     EfUpdate $json.context $startupProject $json.projet
+} 
+
+function EfRemoveMigration($context, $projet) {
+    Write-Host "================Remove migration=========================="
+    Write-Host "Projet :"$projet
+    Write-Host "Context :"$context 
+    Write-Host "======================================================="
+    dotnet ef migrations remove --context $context -s $projet -p $projet 
+}
+
+
+
+function EfAddMigration(  $context, $startupProject, $projet, $migration) {
+  
+    Write-Host "================Add migration=========================="
+    Write-Host "Projet :"$projet
+    Write-Host "StartupProject :"$startupProject
+    Write-Host "Context :"$context
+    Write-Host "Migration :"$migration
+    Write-Host "======================================================="
+    dotnet ef migrations add $migration --context $context -s $startupProject -p $projet
+}
+
+
+
+
+
+
+function EfRebuild( $context, $projet) { 
+    Write-Host "================Rebuild Database=========================="      
+    Write-Host "Projet :"$projet
+    Write-Host "Context :"$context
+    Write-Host "=========================================================="
+    dotnet ef database drop --context $context -s $projet -p $projet
+    dotnet ef database update --context $context -s $projet -p $projet
+}
+
+
+
+
+
+
+
+
+
+function EfRemoveMigrationFromJson($json) {    
+    Write-Host "=========================================="
+    Write-Host "EfRemoveMigrationFromJson : "$json.projet
+    Write-Host "=========================================="
+    
+    EfRemoveMigration $json.context $json.projet
+} 
+
+  
+function EfUpdateFromJson($json) {    
+    Write-Host "=========================================="
+    Write-Host "EfUpdateFromJson : "$json.projet $startupProjec
+    Write-Host "=========================================="
+    
+    if ($json.startupProject) {
+        $startupProject = $json.startupProject
+    }
+    else {
+        $startupProject = $json.projet
+    } 
+
+    EfUpdate $json.context $startupProject $json.projet
+} 
+ 
+function EfRebuildFromJson($json) {    
+    Write-Host "=========================================="
+    Write-Host "EfRebuildFromJson : "$json.projet
+    Write-Host "=========================================="
+    
+    EfRebuild $json.context $json.projet
+} 
+ 
+function EfAddMigrationFromJson($json) {    
+    Write-Host "=========================================="
+    Write-Host "EfAddMigrationFromJson : "$json.projet
+    Write-Host "=========================================="
+
+    if ($json.startupProject) {
+        $startupProject = $json.startupProject
+    }
+    else {
+        $startupProject = $json.projet
+    } 
+    
+    EfAddMigration $json.context $startupProject $json.projet $json.migration
 } 
--- powershell/modules/Krosoft/scripts/ef_projet.ps1
+++ powershell/modules/Krosoft/scripts/ef_projet.ps1
@@ -15,3 +15,37 @@ function EfUpdateProjet($configuration) {
 
 
  
+function EfRebuildProjet($configuration) {    
+    Write-Host "Starting: EfRebuildProjet" -ForegroundColor Green
+    Write-Host "=============================================================================="    
+    $path = Get-Location
+    Write-Host "Path                     : " $path 
+    Write-Host "ProjectName              : " $configuration.projectName 
+    Write-Host "=============================================================================="  
+    foreach ($repo in $configuration.Repositories_migration) {           
+        EfRebuildService $repo.name   
+    }   
+    Write-Host "Finishing: EfRebuildProjet" -ForegroundColor Green
+    Write-Host
+    Write-Host 
+} 
+
+
+ 
+function EfAddMigrationProjet($configuration) {    
+    Write-Host "Starting: EfAddMigrationProjet" -ForegroundColor Green
+    Write-Host "=============================================================================="    
+    $path = Get-Location
+    Write-Host "Path                     : " $path 
+    Write-Host "ProjectName              : " $configuration.projectName 
+    Write-Host "=============================================================================="  
+    foreach ($repo in $configuration.Repositories_migration) {           
+        EfAddMigrationService $repo.name   
+    }   
+    Write-Host "Finishing: EfAddMigrationProjet" -ForegroundColor Green
+    Write-Host
+    Write-Host 
+} 
+
+
+ 
